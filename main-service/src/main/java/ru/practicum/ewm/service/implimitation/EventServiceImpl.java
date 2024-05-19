bfcb67fde1a36ac74583e418f7144b754a9b75ec
Username <gregor2014@yandex.ru>
2024-05-19 14:50:09 +0300

    Дипломный проект часть 2 fix EventServiceImpl.java

--- main-service/src/main/java/ru/practicum/ewm/service/implimitation/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/ewm/service/implimitation/EventServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.service.implimitation;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
